89275718a2c5b986026ea61408a42dcc6a09fd42
muhamad-subkhan <muhamadsubkhan99@gmail.com>
2023-04-04 01:26:15 +0700

    1

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -13,13 +13,15 @@ var DB *gorm.DB
 func Database() {
 
 	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	DB_USER := os.Getenv("DB_USER")
 	DB_HOST := os.Getenv("DB_HOST")
 	DB_PORT := os.Getenv("DB_PORT")
 	DB_NAME := os.Getenv("DB_NAME")
 
+
 	var err error
 
-	dsn := fmt.Sprintf("%s:@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DB_PASSWORD, DB_USER, DB_HOST, DB_PORT, DB_NAME)
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
